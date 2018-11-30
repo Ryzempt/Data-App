1ee42e8dfd5c59c81b77321e956e526727e87c0e
Robbie <robbiekaufman0504@gmail.com>
2018-11-30 07:59:18 -0700

    Initial Commit

---
+++ Data App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data App
+//
+//  Created by Kaufman, Robert on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
